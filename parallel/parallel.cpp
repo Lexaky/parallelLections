9162e5881c60082abe25f9914c7b4000f81f8c1a
Alexey Streltsov <streltcov.ar@dvfu.ru>
2024-10-11 14:10:45 +1000

    Замеры времени работы разных методов

--- parallel/parallel.cpp
+++ parallel/parallel.cpp
@@ -4,6 +4,34 @@
 #include "omp.h"
 #include "stdlib.h"
 
+//В ++ реализовано std::harware_constructive_interference_size и
+//std::harware_destructive_interference_size
+
+//Выравнивание элементов данных
+/*
+	Если данные имеют некоторый виртуальный адрес A (целое число)
+	Адрес должен делиться на некоторое число.
+	Выравнивание данных по D - значит, что A mod D = 0
+	В случае двоичной арифметики:
+	A mod B^d <=> A выравнено по d-символьной границе
+
+	Одним из способов выравнивания данных является создание
+	собственной структуры (type punning)
+	struct some_struct {
+		union {
+			unsigned value;
+			char padding_[64];
+			double c;
+		}
+	}
+	some_struct str;
+	Работа объединения:
+	[*|*|*|*|*|*|*|*]
+	|unsigned|000000| или
+	|double     |000| или
+	|char[64]       | весь массив
+*/
+
 unsigned sum_omp_reduce(std::vector<unsigned> v, unsigned n) {
 	unsigned sum = 0;
 #pragma omp parallel for reduction (+ : sum)
@@ -27,7 +55,7 @@ unsigned sum_round_robin(std::vector<unsigned> v, unsigned n) {
 	unsigned T = 0;
 	// Введём массив сумм каждого потока
 	unsigned* part_sum;
-	
+
 	#pragma omp parallel
 	{
 		unsigned t = omp_get_thread_num();
@@ -50,11 +78,49 @@ unsigned sum_round_robin(std::vector<unsigned> v, unsigned n) {
 	return sum;
 }
 
+//unsigned round_robin(std::vector <unsigned> v, unsigned n) {
+//	n = v.size();
+//#pragma omp parallel 
+//	{
+//		int T = omp_get_num_threads();
+//		int s = n / T;
+//		int b = n % T;
+//		unsigned t = omp_get_thread_num();
+//		if (t < b)
+//			b = ++s * t;
+//		else
+//			b += s * t;
+//		int e = b + s;
+//		for (int i_start = b; i_start < e; i_start++)
+//		{
+//			//part_sum += v[i]
+//		}
+//		//sum = part_sum[0...t]
+//	}
+//	
+//}
+// 
+// Эпоха - это фиксированная дата. Иногда это 1970-й год, 1980-й год и др.
 int main() {
-
-	std::vector <unsigned> vec(1 << 24, 12);
-	std::cout << "sum(vec, n) = " << std::hex << sum(vec, vec.size()) << "\n";
-	std::cout << "sum_omp_reduce(v, n) = " << std::hex << sum_omp_reduce(vec, vec.size()) << "\n";
-	std::cout << "sum_round_robin(v, n) = " << sum_round_robin(vec, vec.size());
+	
+	
+	std::vector <unsigned> vec(1 << 28, 3);
+	double t0 = omp_get_wtime();
+	std::cout << sum_round_robin(vec, vec.size()) << "\n";
+	std::cout << "Took " << (omp_get_wtime() - t0)*1000 << " ms\n";
+	std::fill_n(vec.begin(), vec.size(), 3);
+	t0 = omp_get_wtime();
+	std::cout << sum_omp_reduce(vec, vec.size()) << "\n";
+	std::cout << "Took " << (omp_get_wtime() - t0) * 1000 << " ms\n";
+	std::fill_n(vec.begin(), vec.size(), 3);
+	t0 = omp_get_wtime();
+	std::cout << sum(vec, vec.size()) << "\n";
+	std::cout << "Took " << (omp_get_wtime() - t0) * 1000 << " ms\n";
+	std::fill_n(vec.begin(), vec.size(), 3);
+	//std::cout << "sum(vec, n) = " << std::hex << sum(vec, vec.size()) << "\n";
+	//std::cout << "sum_omp_reduce(v, n) = " << std::hex << sum_omp_reduce(vec, vec.size()) << "\n";
+	//std::cout << "sum_round_robin(v, n) = " << sum_round_robin(vec, vec.size());
+	
 	return 0;
 }
+
