fffa3701d694a9f4ee99fdc5c83c7ac6a2f9b873
Alexey Streltsov <streltcov.ar@dvfu.ru>
2024-11-22 14:16:25 +1000

    parallel скелет добавлен

--- parallel/parallel.cpp
+++ parallel/parallel.cpp
@@ -5,8 +5,61 @@
 #include <chrono>
 #include "num_threads.h"
 #include <fstream>
+#include "omp.h"
 
 static unsigned thread_no = std::thread::hardware_concurrency();
+
+struct partial_t {
+	alignas(std::hardware_destructive_interference_size) // = 40
+		unsigned value;
+};
+
+
+
+unsigned round_robin(std::vector<unsigned> v, unsigned n) {
+	n = v.size();
+	partial_t* part_sum;
+	unsigned T;
+	unsigned sum = 0;
+#pragma omp parallel 
+	{
+		unsigned t = omp_get_thread_num();
+		unsigned s, b, e;
+#pragma omp single 
+		{
+			T = omp_get_num_threads();
+			part_sum = (partial_t*)malloc(sizeof(partial_t) * T);
+			s = n / T;
+			b = n % T;
+		}
+
+		part_sum[t].value = 0;
+
+		if (t < b)
+		{
+			b = (s + 1) * t;
+			e = b + s + 1; 
+		}
+		else
+		{
+			b = b + s * t;
+			e = b + s;
+		}
+
+		for (unsigned i = b; i < e; i++)
+		{
+			part_sum[t].value += v[i];
+		}
+
+	}
+
+	for (int j = 0; j < T; j++)
+	{
+		sum += part_sum[j].value;
+	}
+	free(part_sum);
+	return sum;
+}
 
 unsigned sum_c_mutex(unsigned* v, unsigned v_size) {
 	unsigned sum = 0;
