91e652251620a38328eaf7ecb7776e38391bda5f
Alexey Streltsov <streltcov.ar@dvfu.ru>
2024-12-06 14:31:48 +1000

    Доделан метод быстрого возведения в степень, реализованы own классы моноида и Z для быстрого возведения в степень

--- parallel/parallel.cpp
+++ parallel/parallel.cpp
@@ -8,6 +8,7 @@
 #include <thread>
 #include "ExperimentsRunner.cpp"
 #include "Sums.h"
+#include "RandomizeGenerator.h"
 
 
 // Method to create csv file from vector that contains scalability_result objects
@@ -18,19 +19,16 @@ void to_csv(std::ostream& io, std::vector<scalability_result> v) {
 	}
 }
 int main() {
-	auto sr = run_experiment(sum_cpp_mutex, 1u, 1u<<28);
+	/*auto sr = run_experiment(sum_cpp_mutex, 1u, 1u<<28);
 
 	std::ofstream os("file.csv", std::ios_base::out);
 	if (os.is_open()) {
 		to_csv(os, sr);
 		os.close();
-	}
-	unsigned* vect = new unsigned[1000000];
-	for (unsigned i = 0, j = 0; i < 1000000; i++, j++) {
-		vect[j] = i;
-	}
-	std::cout << localization_sum(vect, 1000000);
-
+	}*/
+	
+	Monoid k;
+	std::cout << myPow::pow(Z<1000000u>(5u), 13u).get() << "\n";
 
 	return 0;
 }
